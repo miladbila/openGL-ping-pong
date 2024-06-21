4d5003c36b9cd3ce25c194dea52d1c586175e06a
Milad <milad.tavakoli81@gmail.com>
2024-06-21 12:36:26 +0330

    init timer and reshape call back functions

--- Ping-Pong/main.cpp
+++ Ping-Pong/main.cpp
@@ -4,6 +4,12 @@
 #include <GL/glut.h>
 #include <iostream>
 
+GLfloat ballRadius = 0.05f;
+GLfloat ballXMax, ballXMin, ballYMax, ballYMin;
+GLdouble clipAreaXLeft, clipAreaXRight, clipAreaYBottom, clipAreaYTop;
+int refreshMillis = 16;
+
+
 void display() {
 	glClearColor(0, 0, 0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -12,12 +18,46 @@ void display() {
 	glFlush();
 }
 
+void reshape(GLsizei width, GLsizei height)
+{
+	if (height == 0) height = 1;
+	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (width >= height)
+	{
+		clipAreaXLeft = -1.0 * aspect;
+		clipAreaXRight = 1.0 * aspect;
+		clipAreaYBottom = -1.0;
+		clipAreaYTop = 1.0;
+	}
+	else
+	{
+		clipAreaXLeft = -1.0;
+		clipAreaXRight = 1.0;
+		clipAreaYBottom = -1.0 / aspect;
+		clipAreaYTop = 1.0 / aspect;
+	}
+	gluOrtho2D(clipAreaXLeft, clipAreaXRight, clipAreaYBottom, clipAreaYTop);
+	ballXMin = clipAreaXLeft + ballRadius;
+	ballXMax = clipAreaXRight - ballRadius;
+	ballYMin = clipAreaYBottom + ballRadius;
+	ballYMax = clipAreaYTop - ballRadius;
+}
 
+void timer(int value)
+{
+	glutPostRedisplay();
+	glutTimerFunc(refreshMillis, timer, 0);
+
+}
 int main(int argc, char* argv[]) {
 	glutInit(&argc, argv);
 	glutInitWindowSize(800, 600);
 	glutCreateWindow("Ping Pong");
 	glutDisplayFunc(display);
+	glutTimerFunc(0, timer, 0);
 	glutMainLoop();
 	return 0;
 }
